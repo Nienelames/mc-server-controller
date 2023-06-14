82a73731aa6b82b2bdfe5f0e21db297cff7c0ce2
Karolis Rauckis <k.rauckis@gmail.com>
2023-06-14 18:38:31 +0300

    Add server interactions

---
+++ bot/serverControls.js
@@ -0,0 +1,48 @@
+const WebSocket = require("ws");
+const wol = require("wol");
+const { state, hostEvents } = require("./webSocket");
+const { Rcon } = require("rcon-client");
+
+// VLAN credentials
+const rcon = new Rcon({ host: "172.23.85.11", port: 25575, password: "e" });
+
+module.exports = {
+  isAliveHost: () => state.hostWs && state.hostWs.readyState === WebSocket.OPEN,
+  powerOnHost: () =>
+    new Promise((resolve, reject) => {
+      wol.wake("D8-5E-D3-5F-10-B8", (error, response) => {
+        if (error) reject(error);
+        else hostEvents.on("hostConnected", () => resolve());
+      });
+    }),
+  startServer: () =>
+    new Promise(async (resolve, reject) => {
+      try {
+        await fetch("http://192.168.3.61:3000/start-server");
+      } catch (error) {
+        reject(error);
+
+        return;
+      }
+
+      state.hostWs.on("server-on", () => resolve());
+    }),
+  isAliveServer: async () =>
+    new Promise(async (resolve, reject) => {
+      try {
+        await rcon.connect();
+        await rcon.end();
+
+        resolve(true);
+      } catch (error) {
+        if (error.code === "ECONNREFUSED") {
+          resolve(false);
+
+          return;
+        }
+
+        console.error(error);
+        reject(error);
+      }
+    }),
+};
