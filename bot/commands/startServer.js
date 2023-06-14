6b429700c8c463ef0625280300fa7e6ee89a8e4d
Karolis Rauckis <k.rauckis@gmail.com>
2023-06-14 18:39:40 +0300

    Add and register start server shlash command

---
+++ bot/commands/startServer.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require("discord.js");
+const {
+  isAliveHost,
+  isAliveServer,
+  powerOnHost,
+  startServer,
+} = require("../serverControls");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("start-server")
+    .setDescription("Starts the Enigmatica 6 server"),
+  async execute(interaction) {
+    if (await isAliveServer()) {
+      interaction.reply("Server is already running");
+
+      return;
+    }
+
+    if (isAliveHost()) {
+      await interaction.reply("Starting server :hourglass:");
+      await startServer();
+      await interaction.followUp("Server running!");
+
+      return;
+    }
+
+    await interaction.reply("Powering on host :hourglass:");
+    await powerOnHost();
+    await interaction.followUp("Hosed powered on!");
+
+    await interaction.followUp("Starting server :hourglass:");
+    await startServer();
+    await interaction.followUp("Server running!");
+  },
+};
--- bot/index.js
+++ bot/index.js
@@ -1,16 +1,66 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const { REST, Routes } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const { token, clientId } = require("./config.json");
 const { state, hostEvents } = require("./webSocket");
-const { Server } = require("ws");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.once(Events.ClientReady, (client) => {
   console.log(`Ready! Logged in as ${client.user.tag}`);
   client.user.setActivity("Idling");
 });
 
 client.login(token);
+
+client.commands = new Collection();
+client.commands.set("start-server", require("./commands/startServer.js"));
+const commands = [require("./commands/startServer.js").data.toJSON()];
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(Routes.applicationCommands(clientId), {
+      body: commands,
+    });
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: `There was an error while executing this command\n:${error}`,
+      });
+    } else {
+      await interaction.reply({
+        content: `There was an error while executing this command!\n${error}`,
+      });
+    }
+  }
+});
 
 hostEvents.on("hostConnected", () => {
   state.hostWs.on("message", (message) => {
@@ -27,3 +77,16 @@ hostEvents.on("hostConnected", () => {
     client.user.setPresence("Idling");
   });
 });
+
+// const { Rcon } = require("rcon-client");
+//
+// const test = async () => {
+//   const rcon = new Rcon({ host: "172.23.85.11", port: 25575, password: "e" });
+//
+//   await rcon.connect();
+//   const response = await rcon.send("list");
+//   console.log(response);
+//   rcon.end();
+// };
+//
+// test();
