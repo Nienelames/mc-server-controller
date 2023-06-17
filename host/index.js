89db70a1c95778a752382cd13b487faab4a4bb85
Karolis Rauckis <k.rauckis@gmail.com>
2023-06-18 01:29:16 +0300

    Mover server logic to it's own file

--- host/index.js
+++ host/index.js
@@ -1,61 +1,25 @@
 const fastify = require("fastify")({ logger: true });
-const { spawn, exec, execFile } = require("child_process");
-const path = require("path");
-const WebSocket = require("ws");
-
-let serverProcess = null;
-const ws = new WebSocket("ws://192.168.3.61:1987");
+const { exec } = require("child_process");
+const { startServer, stopServer } = require("./server");
 
 fastify.get("/start-server", async (request, reply) => {
   try {
-    await new Promise((resolve, reject) => {
-      serverProcess = spawn(
-        "cmd.exe",
-        ["/c", path.resolve("./enigmatica6/start-server.bat")],
-        { stdio: "pipe" }
-      );
+    await startServer();
 
-      serverProcess.on("error", (error) => {
-        reject(new Error(`Failed to start server: ${error.message}`));
-        console.error(`Failed to start server: ${error.message}`);
-      });
-
-      serverProcess.stdout.on("data", (data) => {
-        if (
-          data
-            .toString()
-            .includes(
-              "Loading of 'lightnetwork' for world minecraft:overworld finished"
-            )
-        )
-          resolve();
-
-        if (data.toString().includes("Press any key to continue"))
-          serverProcess.kill();
-      });
-
-      serverProcess.on("exit", () => ws.send("server-off"));
-    });
-
-    reply.code(200).send("Server started");
+    reply.code(200).send("Server started successfully");
   } catch (error) {
-    reply.code(500).send(error.message);
+    reply.code(500).send(error);
   }
 });
 
 fastify.get("/stop-server", async (request, reply) => {
-  serverProcess.stdin.write("/stop");
-  serverProcess.stdin.end();
+  try {
+    await stopServer();
 
-  // Hacky way to see if I'm using the PC
-  await exec(
-    'tasklist | rg -m1 "firefox.exe|Discord.exe"',
-    (error, stdout, stderr) =>
-      stdout.toString() === "" &&
-      exec(`powershell.exe -File ${path.resolve("./shutdown.ps1")}`)
-  );
-
-  reply.code(200).send("Server shut down successfully");
+    reply.code(200).send("Server shut down successfully");
+  } catch (error) {
+    reply.code(500).send(error);
+  }
 });
 
 const start = async () => {
