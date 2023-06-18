a443d9a289ff7906d8f6c7800fb6c0a8bee3e366
Karolis Rauckis <k.rauckis@gmail.com>
2023-06-18 21:43:41 +0300

    Fix server process call

--- host/server.js
+++ host/server.js
@@ -2,8 +2,17 @@ const { spawn, exec } = require("child_process");
 const path = require("path");
 const WebSocket = require("ws");
 
-const serverProcess = spawn(path.resolve("enigmatica6/start-server.bat"));
+const serverProcess = spawn("cmd.exe", [
+  "/c",
+  "cd",
+  path.resolve(__dirname, "enigmatica6"),
+  "&&",
+  "start-server.bat",
+]);
 const ws = new WebSocket("ws://192.168.3.60:1987");
+
+serverProcess.stdout.on("data", (data) => console.log(data.toString()));
+serverProcess.stderr.on("data", (data) => console.log(data.toString()));
 
 serverProcess.on(
   "close",
@@ -13,14 +22,19 @@ serverProcess.on("error", (error) => console.error(error));
 
 const startServer = () =>
   new Promise((resolve, reject) => {
+    serverProcess.stdin.write("e");
+
     serverProcess.on("error", (error) => {
       reject(`Failed to start server: ${error.message}`);
     });
 
     serverProcess.stdout.on("data", (data) => {
       if (
-        "Loading of 'lightnetwork' for world minecraft:overworld finished" in
-        data.toString()
+        data
+          .toString()
+          .includes(
+            "Loading of 'lightnetwork' for world minecraft:overworld finished"
+          )
       )
         resolve();
     });
@@ -28,8 +42,7 @@ const startServer = () =>
 
 const stopServer = () =>
   new Promise((resolve, reject) => {
-    serverProcess.stdin.write("/stop");
-    serverProcess.stdin.end();
+    serverProcess.stdin.write("/stop\n");
 
     // Hacky way to see if I'm using the PC
     exec(
@@ -38,7 +51,9 @@ const stopServer = () =>
         if (error) reject(error.message);
 
         if (!stdout.toString())
-          exec(`powershell.exe -File ${path.resolve("./shutdown.ps1")}`);
+          exec(
+            `powershell.exe -File ${path.resolve(__dirname, "shutdown.ps1")}`
+          );
       }
     );
   });
